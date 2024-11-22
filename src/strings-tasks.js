12b392a4797bfe2496fa422365fddc8019e62106
nikitabogdanov <gittry@gmail.com>
2024-11-22 14:57:17 +0300

    feat: make some tests

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -191,8 +191,18 @@ function removeLastOccurrences(/* str, value */) {
  *   sumOfCodes('') => 0
  *   sumOfCodes() => 0
  */
-function sumOfCodes(/* str */) {
-  throw new Error('Not implemented');
+function sumOfCodes(str) {
+  let i = 0;
+  let sum = 0;
+  if (str === '' || str === undefined || str === null) {
+    sum = 0;
+  } else {
+    while (i < str.length && str !== '' && str !== undefined) {
+      sum += str.charCodeAt(i);
+      i += 1;
+    }
+  }
+  return sum;
 }
 
 /**
@@ -206,8 +216,8 @@ function sumOfCodes(/* str */) {
  *   startsWith('Hello World', 'World') => false
  *   startsWith('Hello World', 'Hello') => true
  */
-function startsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function startsWith(str, substr) {
+  return str.startsWith(substr);
 }
 
 /**
@@ -221,8 +231,8 @@ function startsWith(/* str, substr */) {
  *   endsWith('Hello World', 'World') => true
  *   endsWith('Hello World', 'Hello') => false
  */
-function endsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function endsWith(str, substr) {
+  return str.endsWith(substr);
 }
 
 /**
@@ -252,8 +262,14 @@ function formatTime(/* minutes, seconds */) {
  *   reverseString('abcdef') => 'fedcba'
  *   reverseString('12345') => '54321'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  let result = '';
+  let i = 0;
+  while (i < str.length) {
+    result = str[i] + result;
+    i += 1;
+  }
+  return result;
 }
 
 /**
