4102bfaeb0be69278cfa1576bc3910d623dbd4e8
nikitabogdanov <gittry@gmail.com>
2024-12-09 15:45:42 +0300

    feat: make some tests

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -421,8 +421,14 @@ function findLongestWord(/* sentence */) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  const array = str.split(' ');
+  const result = array
+    .map(function reverseWord(element) {
+      return element.split('').reverse().join('');
+    })
+    .join(' ');
+  return result;
 }
 
 /**
